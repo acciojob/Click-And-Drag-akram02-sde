4819be8ca832378f9452a99432bdd20cc96ea982
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-02 13:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 // Your code here.
-// Your code here.
-const items = document.querySelectorAll('.item')
-const container = document.querySelector('.items')
-items.forEach(el=>{
-	// el.draggable = "true"
-	el.addEventListener('mouseup',()=>{
-		container.scrollLeft +=50
-	})
-	el.addEventListener('mousedown',()=>{
-		container.scrollLeft -=50
-	})
-})
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+isDown = true;
+slider.classList.add('active');
+startX = e.pageX - slider.offsetLeft;
+scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+isDown = false;
+slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+isDown = false;
+slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+if (!isDown) return; // stop the fn from running
+e.preventDefault();
+const x = e.pageX - slider.offsetLeft;
+const walk = (x - startX) * 3;
+slider.scrollLeft = scrollLeft - walk;
+});
