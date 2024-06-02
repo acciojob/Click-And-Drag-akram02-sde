b370048420878ec6af9638c18cb214a3b807018e
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-02 12:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Your code here.
+// Your code here.
+const items = document.querySelectorAll('.item')
+const container = document.querySelector('.items')
+items.forEach(el=>{
+	// el.draggable = "true"
+	el.addEventListener('mouseup',()=>{
+		container.scrollLeft +=50
+	})
+	el.addEventListener('mousedown',()=>{
+		container.scrollLeft -=50
+	})
+})
